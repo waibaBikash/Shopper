342a8c938c13ef0052bfadcf7614c643426e5904
waibaBikash <kavrelibikash@gmail.com>
2024-06-13 18:24:44 +1000

    create a signup gage

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const LoginSignup = () => {
   return (
-    <div>
-
+    <div className='loginsignup'>
+        <div className="loginsignup-container">
+           <h1>Sign Up</h1>
+            <div className="loginsignup-fields">
+               <input type="text" placeholder='Your Name' />
+               <input type="email" placeholder='Your Email' />
+               <input type="password" placeholder='Your Pssword' />
+            </div>
+        </div>
     </div>
   )
 }
