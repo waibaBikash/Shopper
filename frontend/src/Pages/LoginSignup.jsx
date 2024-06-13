69adec6fee45945a545bb0c65c926378ec84d841
waibaBikash <kavrelibikash@gmail.com>
2024-06-13 18:48:02 +1000

    complete signup page

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
@@ -7,9 +8,15 @@ const LoginSignup = () => {
            <h1>Sign Up</h1>
             <div className="loginsignup-fields">
                <input type="text" placeholder='Your Name' />
-               <input type="email" placeholder='Your Email' />
-               <input type="password" placeholder='Your Pssword' />
+               <input type="email" placeholder='Email Address' />
+               <input type="password" placeholder='Password' />
             </div>
+             <button>Continue</button>
+             <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+             <div className="loginsignup-agree">
+               <input type="checkbox"  name='' id=''/>
+               <p>By counting, i agree to the terms of use & privacy policy</p>
+             </div>
         </div>
     </div>
   )
