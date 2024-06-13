fa68155bc1f55e00aa890973d48c4b74274fb0c4
waibaBikash <kavrelibikash@gmail.com>
2024-06-13 17:14:40 +1000

    save change

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className='footer'>
       <div className="footer-logo">
          <img src={footer_logo} alt="" />
-         <p>SHOOPER</p>
+         <p>WAIBASHOP</p>
       </div>
         <ul className="footer-links">
            <li>Company</li>
