c69e8719fc164a2dceadb2b68759601d8dc71565
waibaBikash <kavrelibikash@gmail.com>
2024-06-14 14:02:59 +1000

    complete product display page

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -19,9 +19,9 @@ const ProductDisplay = (props) => {
                <img className='productdisplay-main-img' src={product.image} alt="" />
             </div>
          </div>
-         <div className="productdisplay right">
+         <div className="productdisplay-right">
            <h1>{product.name}</h1>
-           <div className="productdisplay-right-star">
+           <div className="productdisplay-right-stars">
              <img src={star_icon} alt="" />
              <img src={star_icon} alt="" />
              <img src={star_icon} alt="" />
@@ -38,11 +38,11 @@ const ProductDisplay = (props) => {
                </div>
             </div>
              <div className="productdisplay-right-description">
-              A lightweight, usually knittted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
+              A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.
              </div>
                <div className="porductdisplay-right-size">
                 <h1>Select Size</h1>
-                  <div className="productdisplay-right-size">
+                  <div className="productdisplay-right-sizes">
                     <div>S</div>
                     <div>M</div>
                     <div>L</div>
