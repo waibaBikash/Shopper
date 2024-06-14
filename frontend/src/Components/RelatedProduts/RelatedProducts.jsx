87d2680268aed7fdb769822c80709f507428889e
waibaBikash <kavrelibikash@gmail.com>
2024-06-14 14:55:10 +1000

    create related product

---
+++ frontend/src/Components/RelatedProduts/RelatedProducts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './RelatedProducts.css';
+
+
+const RelatedProducts = () => {
+  return (
+    <div className='relatedproducts'>
+           <h1>Related Products</h1>
+           <hr />
+           <div className="relatedproducts-item">
+
+           </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
