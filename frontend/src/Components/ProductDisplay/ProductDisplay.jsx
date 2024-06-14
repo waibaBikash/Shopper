c594b3bbb682df6f5bf7259aec5bbd2a10c5fa7d
waibaBikash <kavrelibikash@gmail.com>
2024-06-14 13:34:23 +1000

    save changes

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -7,7 +7,7 @@ const ProductDisplay = (props) => {
 
   const {product} = props;
   return (
-    <div className='porductdisplay'>
+    <div className='productdisplay'>
          <div className="productdisplay-left">
            <div className="productdisplay-img-list">
             <img src={product.image} alt="" />
