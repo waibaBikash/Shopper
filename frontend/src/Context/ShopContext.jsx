044b6ba0726199381eacb8cc96cdc8edcda59e9e
waibaBikash <kavrelibikash@gmail.com>
2024-06-22 19:58:06 +1000

    complete all functionality

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -34,7 +34,17 @@ const ShopContextProvider = (props) => {
       }
   }
 
-  const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount};
+  const getTotalCartItems = () => {
+    let totalItems = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+         totalItems += cartItems[item]
+      }
+      return totalItems;
+    }
+  }
+
+  const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount,getTotalCartItems};
   
   return (
     <ShopContext.Provider value={contextValue}>
