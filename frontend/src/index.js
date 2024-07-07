81cac677cce15105365a7011fd4ce1f1caa068e7
waibaBikash <kavrelibikash@gmail.com>
2024-07-07 20:01:46 +1000

    commit change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import ShopContextProvider from './Context/ShopContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
