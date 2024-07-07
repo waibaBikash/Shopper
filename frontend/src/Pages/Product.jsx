5aff37eec2505526b45c35e698212b6fb6b4b0ef
waibaBikash <kavrelibikash@gmail.com>
2024-07-07 20:04:50 +1000

    save change

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -7,7 +7,6 @@ import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProduts/RelatedProducts';
 
 const Product = () => {
-
   const {all_product} = useContext(ShopContext);
   const {productId} = useParams();
   const product = all_product.find((e)=>e.id === Number(productId));
